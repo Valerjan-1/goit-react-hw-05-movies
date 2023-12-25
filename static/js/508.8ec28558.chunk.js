cef4a011faf1cdf2020872d273c75a820308a793
Valerjan-1 <134737688+Valerjan-1@users.noreply.github.com>
2023-12-25 20:56:55 +0000

    Deploying to gh-pages from @ Valerjan-1/goit-react-hw-05-movies@25e5c4ad62f4bbb8eb224ed5ddbc601fc5c2c53f 🚀

---
+++ static/js/508.8ec28558.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[508],{508:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});t(791);var r=t(87),s=t(689),a={},c=t(184),o=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsxs)("nav",{className:a.nav,children:[(0,c.jsx)(r.OL,{to:"/",end:!0,children:"Home"}),(0,c.jsx)(r.OL,{to:"/movies",children:"Movies"})]}),(0,c.jsx)(s.j3,{})]})}}}]);
+//# sourceMappingURL=508.8ec28558.chunk.js.map
